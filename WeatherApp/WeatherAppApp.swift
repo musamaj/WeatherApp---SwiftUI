640ea2eb95f20d5cfd1d2d12227787e159c12518
Usama Jamil <usama_csuet@hotmail.com>
2025-03-30 02:59:49 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Usama Jamil on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Usama Jamil on 3/30/25.
+//
+
+import Testing
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
